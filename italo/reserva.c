526e3cefe3d1f30e7bc9cb8ad075950f1cb6bfc6
Italo Vinhas <italovas05@outlook.com>
2024-11-24 00:25:22 -0300

    Iniciando o sistema de reserva

---
+++ italo/reserva.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 200
+//preciso fazer com que  sistema reserve um voo para um passageiro, e mostrar os voos, cadeiras apartir do destino do passageiro//
+
+typedef struct{
+    int assentos[2][MAX];
+    int numAviao;
+    char nomePassageiro[MAX];
+}Informacao;
+
+int main(){
+    Informacao i;
+
+    
+}
